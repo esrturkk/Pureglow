ac3b8ad011d0953da28dcf2dca2d619714077bca
esrturkk <esrturkk1645@gmail.com>
2024-05-04 12:03:33 +0300

    Proje eklendi

---
+++ bakim/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+
+def aboutus(request):
+    return render(request, 'aboutus.html')
+
+def homeng(request):
+    return render(request, 'homeng.html')
+
+def home(request):
+    return render(request, 'home.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def products(request):
+    return render(request, 'products.html')
+
+def signup(request):
+    return render(request, 'signup.html')
+
+def test(request):
+    return render(request, 'test.html')
+
+def tips(request):
+    return render(request, 'tips.html')
+
